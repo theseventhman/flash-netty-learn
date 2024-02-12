5b74a40b89633c7561b7dd1787b5103d6520eedd
tcn741 <tianchenlko@126.com>
2024-02-12 22:19:18 +0800

    初始化

--- src/main/java/com/tj/exercise/flash/netty/learn/chapter2/NettyServer.java
+++ src/main/java/com/tj/exercise/flash/netty/learn/chapter2/NettyServer.java
@@ -8,6 +8,8 @@ import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.string.StringDecoder;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
 
 /**
  * @Author: tj
@@ -15,23 +17,27 @@ import io.netty.handler.codec.string.StringDecoder;
  */
 public class NettyServer {
     public static void main(String[] args){
-        ServerBootstrap serverBootstrap = new ServerBootstrap();
-        NioEventLoopGroup boss = new NioEventLoopGroup();
-        NioEventLoopGroup worker = new NioEventLoopGroup();
 
-        serverBootstrap.group(boss,worker)
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+
+        serverBootstrap.group(bossGroup,workerGroup)
                        .channel(NioServerSocketChannel.class)
                        .childHandler(new ChannelInitializer<NioSocketChannel>() {
                            protected  void initChannel(NioSocketChannel ch){
-                               ch.pipeline().addLast(new StringDecoder());
-                               ch.pipeline().addLast(new SimpleChannelInboundHandler<String>() {
-                                  @Override
-                                   protected  void channelRead0(ChannelHandlerContext ctx, String msg){
-                                      System.out.println(msg);
-                                  }
 
-                               });
                            }
-                       }).bind(8000);
+                       });
+        serverBootstrap.bind(8000).addListener(new GenericFutureListener<Future<? super Void>>() {
+            @Override
+            public void operationComplete(Future<? super Void> future) throws Exception {
+                if(future.isSuccess()){
+                    System.out.println("端口绑定成功!");
+                } else{
+                    System.out.println("端口绑定失败!");
+                }
+            }
+        });
     }
 }
